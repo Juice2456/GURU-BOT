066a8ccb9127cb5a98e80857ba5a7e4dcc5c7b5a
Juice2456 <149042279+Juice2456@users.noreply.github.com>
2023-11-12 12:44:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ import axios from 'axios'
 
 global.owner = [
   ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2348110961623', 'Ash', true], 
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "REBORN" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
+global.mods = ['917605902011','2348110961623'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '™ʙᴛᴅ✓𝚁𝙴𝙱𝙾𝚁𝙽'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = '™𝚁𝙴𝙱𝙾𝚁𝙽' 
+global.author = '@Joel' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
